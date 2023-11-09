e0e87d3fd3032d3ec7a8860e746d053e2af42f24
Lorena González <gonzalezm.lorena2@gmail.com>
2023-11-09 20:49:46 -0300

    inicio del proyecto

---
+++ primo.js
@@ -0,0 +1,22 @@
+function primo(num) {
+    var p = 0;
+    for (var i = 1; i <= num; i = i + 1) {
+
+        if (num % i == 0) {
+            p = p + 1
+        }
+    }
+ 
+    if (p > 2)
+        console.log("El número "+num+" no es primo estúpida.");
+
+    else
+        console.log("El numero: "+num+" : Es primo");
+
+    
+//p>2? console.log("El número "+num+" no es primo estúpida."): console.log("El numero: "+num+" : Es primo");
+
+
+    
+}
+primo(8);
