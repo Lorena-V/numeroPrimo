5fb38e35779959ecfef3ba3fce5b9c353275f335
juanOrmeno <juan.pablo.ormeno.farias@gmail.com>
2023-11-10 19:26:14 -0300

    se modifica la funcion primo

--- primo.js
+++ primo.js
@@ -1,29 +1,44 @@
 
 
+
 function primo() {
-    var consultaNumero = document.getElementById("input_num").value;
-    var num = consultaNumero;
-    console.log(consultaNumero);
+    let num = document.getElementById("input_num").value;
+
     if (!isNaN(num)) {
-        document.getElementById("input_num").disabled = true;
-        document.getElementById("btn").disabled = true;
-        
-        var p = 0;
-        for (var i = 1; i <= num; i++) {
-            if (num % i == 0) {
-                p = p + 1;
-            }
-        }
-        if (p > 2){
-            document.querySelector(".escondidorojo").style.visibility="visible";
-            console.log("El número "+num+" no es primo estúpida.");
-        }
-        else
-            document.querySelector(".escondidoverde").style.visibility="visible";
-            console.log("El numero: "+num+" : Es primo");
-        }
+
+        let seraPrimo = esPrimo(num)
+        mostrarPrimo(seraPrimo)
+    }
     else {
         alert("No se permiten letras ni caracteres, por favor vuelva a intentarlo.");
-    }    
+    }
 }
 
+function esPrimo(num) {
+    p = 0
+    for (let i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            p++
+        }
+    }
+
+    if (p == 2)
+        return true
+    else
+        return false
+
+}
+
+
+function mostrarPrimo(siEs) {
+
+    if (siEs) {
+        document.querySelector(".escondidoverde").style.visibility = "visible";
+        document.querySelector(".escondidorojo").style.visibility = "hidden";
+    } else {
+        document.querySelector(".escondidoverde").style.visibility = "hidden";
+        document.querySelector(".escondidorojo").style.visibility = "visible";
+    }
+
+
+}
