be64bf8d2911b8c450f17018743b0f315113df8e
Lorena González <gonzalezm.lorena2@gmail.com>
2023-11-10 16:54:31 -0300

    ejecucion de la funcion numero primo

--- primo.js
+++ primo.js
@@ -1,16 +1,26 @@
 
-let num = 8;
+
 function primo() {
-    var p = 0;
-    for (var i = 1; i <= num; i = i + 1) {
-        if (num % i == 0) {
-            p = p + 1
+    var consultaNumero = document.getElementById("input_num").value;
+    let num = consultaNumero
+    console.log(consultaNumero);
+    if (num == Number) {
+        var p = 0;
+        for (var i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                p = p + 1
+            }
         }
-    }
- 
-    if (p > 2)
-        console.log("El número "+num+" no es primo estúpida.");
-    else
-        console.log("El numero: "+num+" : Es primo");
+        if (p > 2){
+            document.querySelector(".escondidorojo").style.visibility="visible";
+            console.log("El número "+num+" no es primo estúpida.");
+        }
+        else
+            document.querySelector(".escondidoverde").style.visibility="visible";
+            console.log("El numero: "+num+" : Es primo");
+        }
+    else {
+        alert("No se permiten letras ni caracteres, por favor vuelva a intentarlo.")
+    }    
 }
 
